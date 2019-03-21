f528209f3f1574883eb2f838ba7dac07618c6dbf
rabiulhasan3 <rabiul.fci@gmail.com>
2019-03-22 01:29:32 +0600

    UsersTableSeeder And RoleTableSeeder Created

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+        	'role_id' => 1,
+        	'name' => 'Mr. Admin',
+        	'username' => 'admin',
+        	'email' => 'admin@blog.com',
+        	'password' => bcrypt('123456'),
+        ]);
+
+        DB::table('users')->insert([
+        	'role_id' => 2,
+        	'name' => 'Mr. Author',
+        	'username' => 'author',
+        	'email' => 'author@blog.com',
+        	'password' => bcrypt('123456'),
+        ]);
+    }
+}
