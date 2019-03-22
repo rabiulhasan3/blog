e70b8ecec10b54b7e2c01daa7af695ce186b1158
rabiulhasan3 <rabiul.fci@gmail.com>
2019-03-22 16:31:56 +0600

    Admin And Author Route group and dashboard setup

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,18 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+// Admin All Route Here
+Route::group(['prefix'=>'admin','namespace'=>'Admin','middleware'=>['auth','admin'],'as'=>'admin.'],function(){
+
+	Route::get('dashboard','DashboardController@index')->name('dashboard');
+
+});
+
+
+//Author All Route Here
+Route::group(['prefix'=>'author','namespace'=>'Author','middleware'=>['auth','author'],'as'=>'author.'],function(){
+
+	Route::get('dashboard','DashboardController@index')->name('dashboard');
+
+});
