7375c404dec371a8ea901ea8916b250b7cc0eb5c
rabiulhasan3 <rabiul.fci@gmail.com>
2019-04-02 02:02:17 +0600

    Author Dashboard Setup

--- app/Http/Controllers/Author/DashboardController.php
+++ app/Http/Controllers/Author/DashboardController.php
@@ -4,10 +4,25 @@ namespace App\Http\Controllers\Author;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Auth;
 
 class DashboardController extends Controller
 {
     public function index(){
-    	return view('author.dashboard');
+    	$user = Auth::user();
+    	$posts = $user->posts();
+    	$popular_posts = $user->posts()
+    			->withCount('comments')
+    			->withCount('favorite_to_users')
+    			->orderBy('view_count','desc')
+    			->orderBy('comments_count')
+    			->orderBy('favorite_to_users_count')
+    			->take(5)
+    			->get();
+
+    	$total_pending_post = Auth::user()->posts()->where('is_approved',false)->count();
+    	$total_view = Auth::user()->posts()->sum('view_count');
+
+    	return view('author.dashboard',compact('user','posts','popular_posts','total_pending_post','total_view'));
     }
 }
